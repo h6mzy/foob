f8ffab3d4a2d9c8083685b17c00d5c262172980b
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-07-15 15:48:08 +0800

    Update index.js

--- data/demo/index.js
+++ data/demo/index.js
@@ -1,4 +1,5 @@
 import clubs from './clubs.json' with { type: 'json' };
+import fixtures from './fixtures.json' with { type: 'json' };
 import players from './players.json' with { type: 'json' };
 
-export { clubs, players };
+export { clubs, fixtures, players };
