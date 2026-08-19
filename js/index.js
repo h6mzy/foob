437acb8309c83d60e0354f6e119bd60f4f8e6359
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-19 15:10:36 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,8 +2,13 @@ import ClubCard from './ui/club-card.js';
 import NextMatch from './ui/next-match.js';
 import PlayerCard from './ui/player-card.js';
 
+import { createElement } from './utils/createElement.js';
+import { createImg } from './utils/createImg.js';
+
 export {
   ClubCard,
   NextMatch,
-  PlayerCard
+  PlayerCard,
+  createElement,
+  createImg
 };
