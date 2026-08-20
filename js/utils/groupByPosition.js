91b28feaae5f27c4a158fa2b57821797d10069b5
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-20 13:53:17 +0800

    Create groupByPosition.js

---
+++ js/utils/groupByPosition.js
@@ -0,0 +1,16 @@
+export function getPlayers(players) {
+  const grouped = { goal: [], defence: [], midfield: [], forward: [] };
+
+  players.forEach(player => {
+    const pos = player.position?.trim().toLowerCase();
+    
+    if (!grouped[pos]) {
+      console.warn('Invalid position:', player.name, player.position);
+      return;
+    }
+
+    grouped[pos].push(player);
+  });
+
+  return grouped;
+}
