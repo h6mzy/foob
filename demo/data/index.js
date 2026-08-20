4b95bb27ef48614ff7061ba6dc12f088a22846e6
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-20 15:34:22 +0800

    Update index.js

--- demo/data/index.js
+++ demo/data/index.js
@@ -1,21 +1,13 @@
 import clubs from './clubs.json' with { type: 'json' };
 import competitions from './competitions.json' with { type: 'json' };
-import dkh_clubs from './dkh_clubs.json' with { type: 'json' };
-import dkh_players from './dkh_players.json' with { type: 'json' };
-import fixtures from './fixtures.json' with { type: 'json' };
 import matches from './matches.json' with { type: 'json' };
-import myclub from './myclub.json' with { type: 'json' };
 import partners from './partners.json' with { type: 'json' };
 import players from './players.json' with { type: 'json' };
 
 export {
   clubs,
   competitions,
-  dkh_clubs,
-  dkh_players,
-  fixtures,
   matches,
-  myclub,
   partners,
   players
 };
