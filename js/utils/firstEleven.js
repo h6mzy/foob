efdabaee1fbfa303c01858b0d4db86f629da65c8
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-20 14:11:10 +0800

    Create firstEleven.js

---
+++ js/utils/firstEleven.js
@@ -0,0 +1,32 @@
+import { groupByPostition, sortLineup } from '../index.js';
+import { pickRandom } from 'https://cdn.jsdelivr.net/gh/h6mzy/bits@1.11.8/js/index.js';
+
+// sort by 'c' = captain first, then g, then by number
+// sort by 'g' = by g, then number
+// sort by 'n' = by number
+
+export function firstEleven(players, clubMatchData, sort = 'c') {
+  const [d, m, f] = clubMatchData.formation.split('').map(Number);
+
+  const grouped = groupByPostition(players);
+
+  const lineup = clubMatchData.lineup
+    ? players.filter(p => clubMatchData.firstEleven.includes(p.number))
+    : [
+        ...pickRandom(grouped.goal, 1),
+        ...pickRandom(grouped.defence, d),
+        ...pickRandom(grouped.midfield, m),
+        ...pickRandom(grouped.forward, f),
+      ];
+
+  const matchCaptain =
+    clubMatchData.matchCaptain ||
+    pickRandom(lineup, 1)[0]?.number;
+
+  const captainAssigned = lineup.map(player => ({
+    ...player,
+    isCaptain: player.number === matchCaptain
+  }));
+
+  return sortLineup(captainAssigned, sort);
+};
