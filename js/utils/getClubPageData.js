3b515a5c80bc50c0327a498a35ff64a0e2813738
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-21 10:04:55 +0800

    Update getClubPageData.js

--- js/utils/getClubPageData.js
+++ js/utils/getClubPageData.js
@@ -1,24 +1,38 @@
-import { clubs, matches, players } from '../../demo/data/index.js';
+import { clubsMap, matches, players } from '../../demo/data/index.js';
 import { getFirstEleven, getNextMatch, getRemainingPlayers } from '../index.js';
 
 export function getClubPageData(abbr) {
-  const clubsMap = new Map(clubs.map(c => [c.abbr, c]));
   const club = clubsMap.get(abbr);
   const clubPlayers = players.filter(p => p.clubAbbr === abbr);
   const clubMatches = matches.filter(m => m.home === abbr || m.away === abbr);
   const nextMatch = getNextMatch(clubMatches);
-  const nextMatchNature = nextMatch.home === abbr ? 'home' : 'away';
-  const nextMatchData = nextMatch[`${nextMatchNature}Data`];
+  const enrichedNextMatch = nextMatch ? enrichMatch(nextMatch) : null;
   const firstEleven = getFirstEleven(clubPlayers, nextMatchData);
   const remainingPlayers = getRemainingPlayers(clubPlayers, firstEleven);
-
+  
   return {
     club,
-    players: clubPlayers,
     matches: clubMatches,
-    nextMatch,
-    nextMatchData,
+    nextMatch: enrichedNextMatch,
     firstEleven,
     remainingPlayers
-  }
-};
+  };
+}
+
+function enrichMatch(match) {
+  const { homeData, awayData, ...rest } = match;
+
+  return {
+    ...rest,
+    home: {
+      ...homeData,
+      ...clubsMap.get(match.home),
+      abbr: match.home
+    },
+    away: {
+      ...awayData,
+      ...clubsMap.get(match.away),
+      abbr: match.away
+    }
+  };
+}
