b9e18abf620d331ab88b38ca127283bba43d8d66
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-21 15:31:20 +0800

    Update getClubPageData.js

--- js/utils/getClubPageData.js
+++ js/utils/getClubPageData.js
@@ -1,27 +1,26 @@
 import { clubsMap, matches, players } from '../../demo/data/index.js';
-import { getFirstEleven, getNextMatch, getRemainingPlayers } from '../index.js';
+import { getMatchdaySquad, getNextMatch } from '../index.js';
 
 export function getClubPageData(abbr) {
   const club = clubsMap.get(abbr);
   const clubPlayers = players.filter(p => p.clubAbbr === abbr);
+  
   const clubMatches = matches.filter(m => m.home === abbr || m.away === abbr);
   const nextMatch = getNextMatch(clubMatches);
   const enrichedNextMatch = nextMatch ? enrichMatch(nextMatch) : null;
+  
   const nature = abbr === nextMatch.home ? 'home' : 'away';
-  const firstEleven = getFirstEleven(clubPlayers, enrichedNextMatch[nature]);
-  const remainingPlayers = getRemainingPlayers(
-    clubPlayers,
-    firstEleven,
-    enrichedNextMatch[nature]?.unavailable || []
-  );
+
+  const { firstEleven, remaining, unavailable } = getMatchdaySquad(players, clubMatchData);
   
   return {
     club,
     nextMatch: enrichedNextMatch,
     firstEleven,
-    remainingPlayers
+    remaining,
+    unavailable
   };
-}
+};
 
 function enrichMatch(match) {
   const { homeData, awayData, ...rest } = match;
@@ -39,4 +38,4 @@ function enrichMatch(match) {
       abbr: match.away
     }
   };
-}
+};
