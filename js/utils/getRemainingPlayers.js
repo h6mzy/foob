a06b90ba2ee049c89e9c1ce950ca5280f5bdc86a
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-21 14:27:57 +0800

    Update getRemainingPlayers.js

--- js/utils/getRemainingPlayers.js
+++ js/utils/getRemainingPlayers.js
@@ -1,3 +1,5 @@
+import { sortLineup } from '../../index.js'
+
 export function getRemainingPlayers(
   players,
   firstEleven,
@@ -6,10 +8,14 @@ export function getRemainingPlayers(
   const firstElevenNumbers = new Set(firstEleven.map(p => p.number));
   const unavailableMap = new Map(unavailable.map(u => [u.number, u.reason]));
 
-  return players
-    .filter(player => !firstElevenNumbers.has(player.number))
-    .map(player => ({
-      ...player,
-      unavailable: unavailableMap.get(player.number) || null
-    }));
+  return sortLineup(
+    players
+      .filter(player => !firstElevenNumbers.has(player.number))
+      .map(player => ({
+        ...player,
+        unavailable: unavailableMap.get(player.number) || null
+      })),
+    'n',
+    unavailable
+  );
 }
