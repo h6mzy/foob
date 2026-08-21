0b574b5eff5e44eb28278f62f2a500bf85130dbc
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-21 15:32:43 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,11 +5,10 @@ import PlayerCard from './ui/player-card.js';
 import { createElement } from './utils/createElement.js';
 import { createImg } from './utils/createImg.js';
 import { getClubPageData } from './utils/getClubPageData.js';
-import { getFirstEleven } from './utils/getFirstEleven.js';
+import { getMatchdaySquad } from './utils/getMatchdaySquad.js';
 import { getNextMatch } from './utils/getNextMatch.js';
 import { getOpponents } from './utils/getOpponents.js';
 import { getPastMatches } from './utils/getPastMatches.js';
-import { getRemainingPlayers } from './utils/getRemainingPlayers.js';
 import { groupByPosition } from './utils/groupByPosition.js';
 import { sortLineup } from './utils/sortLineup.js';
 
@@ -20,11 +19,10 @@ export {
   createElement,
   createImg,
   getClubPageData,
-  getFirstEleven,
+  getMatchdaySquad,
   getNextMatch,
   getOpponents,
   getPastMatches,
-  getRemainingPlayers,
   groupByPosition,
   sortLineup
 };
