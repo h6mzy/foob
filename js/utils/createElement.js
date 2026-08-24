cfaef3ab66d08d2a5c083da75b1bc1aa4dfd5752
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-24 16:09:44 +0800

    Delete js/utils/createElement.js

--- js/utils/createElement.js
+++
@@ -1,8 +0,0 @@
-export const createElement = (element, props = {}) => {
-  const el = document.createElement(element);
-  for (const [key, value] of Object.entries(props)) {
-    if (key === 'dataset') Object.assign(el.dataset, value);
-    else el[key] = value;
-  }
-  return el;
-};
