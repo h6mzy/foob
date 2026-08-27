8846ede4670a459d50a51edd4b3b6c72175bd5b1
h6mzy <112537750+h6mzy@users.noreply.github.com>
2026-08-27 15:27:55 +0800

    Update createImg.js

--- js/utils/createImg.js
+++ js/utils/createImg.js
@@ -3,14 +3,13 @@ import { imageExists } from '../index.js';
 export const createImg = (
   src,
   alt,
-  className = 'foob-img',
-  fallbackSrc = '/demo/img/placeholder.webp'
+  className = 'foob-img'
 ) => {
   const article = document.createElement('article');
   article.className = className;
 
   const img = document.createElement('img')
-  img.src =  await imageExists(src, fallbackSrc);
+  img.src =  await imageExists(src);
   img.alt = alt;
 
   article.append(img);
